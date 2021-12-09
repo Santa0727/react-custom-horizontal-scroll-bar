c32c6d56f63e04a60834ea2b93b380d1cbde69c7
Santa0727 <santa0727@outlook.com>
2021-12-08 21:01:15 -0500

    feat: scrolling

---
+++ src/horizontal-bar.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import arrow_left_png from "./arrow-left.png";
+import arrow_right_png from "./arrow-right.png";
+import sectionItems from "./section-items";
+
+const maximumMarginLeft = 650;
+const defaultTitle = "Section a point in my journey to learn more";
+
+const NumberItem = ({ item, setTitle }) => {
+  const [isOn, setIsOn] = useState(false);
+
+  const hover = () => {
+    setTitle(item.title);
+    setIsOn(true);
+  };
+  const leave = () => {
+    setTitle(null);
+    setIsOn(false);
+  };
+
+  return (
+    <div className="number-item" onMouseOver={hover.bind(this)} onMouseLeave={leave.bind(this)}>
+      {isOn ? (
+        <>
+          <div className="number-point strong-point"></div>
+          <div className="number-value">
+            <img src={item.png} alt="item.label" />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="number-point"></div>
+          <div className="number-value">{item.label}</div>
+        </>
+      )}
+    </div>
+  );
+};
+
+const NavNoHover = () => {
+  const [title, setTitle] = useState();
+  const [pos, setPos] = useState(0);
+  const [showArrow, setShowArrow] = useState(false);
+
+  const moveRight = () => {
+    setPos(Math.min(pos + 15, maximumMarginLeft));
+  };
+  const moveLeft = () => {
+    setPos(Math.max(0, pos - 15));
+  };
+  const onWheel = (evt) => {
+    if (evt.deltaY > 0) moveRight();
+    else moveLeft();
+  };
+  const mouseOver = (evt) => {
+    setShowArrow(true);
+  };
+  const mouseLeave = (evt) => {
+    setShowArrow(false);
+  };
+
+  return (
+    <div className="nav-no-hover" onMouseOver={mouseOver.bind(this)} onMouseLeave={mouseLeave.bind(this)}>
+      <div className="point-label">{title ?? defaultTitle}</div>
+      <div className={`arrow-line ${showArrow ? "" : "hide"}`}>
+        <img src={arrow_left_png} alt="arrow-left" onMouseUp={moveLeft.bind(this)} onMouseDown={moveLeft.bind(this)} />
+        <img src={arrow_right_png} alt="arrow-right" onMouseUp={moveRight.bind(this)} onMouseDown={moveRight.bind(this)} />
+      </div>
+      <div className="numbers-line" onWheel={onWheel.bind(this)}></div>
+      <div className="numbers-body" style={{ marginLeft: -pos }} onWheel={onWheel.bind(this)}>
+        {sectionItems.map((item) => (
+          <NumberItem key={item.id} item={item} setTitle={setTitle} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const HorizontalBar = () => {
+  return (
+    <div className="horizontal-bar">
+      <NavNoHover />
+    </div>
+  );
+};
+
+export default HorizontalBar;
---
+++ src/section-items.js
@@ -0,0 +1,54 @@
+import s1_png from "./fork-in-load.png";
+import s2_png from "./lipstick.png";
+import s3_png from "./ramen.png";
+import s4_png from "./clock.png";
+import s5_png from "./hand.png";
+import s6_png from "./lane.png";
+import s7_png from "./everything.png";
+
+const sectionItems = [
+  {
+    id: 1,
+    label: "01",
+    png: s1_png,
+    title: "A change of plans",
+  },
+  {
+    id: 2,
+    label: "02",
+    png: s2_png,
+    title: "Mary kay cosmetics",
+  },
+  {
+    id: 3,
+    label: "03",
+    png: s3_png,
+    title: "Ramen noodles and a dream",
+  },
+  {
+    id: 4,
+    label: "04",
+    png: s4_png,
+    title: "Full time again (This time for keeps)",
+  },
+  {
+    id: 5,
+    label: "05",
+    png: s5_png,
+    title: "My personal truths",
+  },
+  {
+    id: 6,
+    label: "06",
+    png: s6_png,
+    title: "A lane all my own",
+  },
+  {
+    id: 7,
+    label: "07",
+    png: s7_png,
+    title: "Next level everything",
+  },
+];
+
+export default sectionItems;
