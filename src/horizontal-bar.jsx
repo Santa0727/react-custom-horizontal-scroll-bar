b672f889f41973bb1d05f7ccff4fe873782ee522
Santa0727 <santa0727@outlook.com>
2021-12-11 14:41:30 -0500

    feat: custom slick slider

--- src/horizontal-bar.jsx
+++ src/horizontal-bar.jsx
@@ -1,10 +1,33 @@
 import React, { useEffect, useRef, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import arrow_left_png from "./arrow-left.png";
 import arrow_right_png from "./arrow-right.png";
 import sectionItems from "./section-items";
 
-const maximumMarginLeft = 850;
 const defaultTitle = "Section a point in my journey to learn more";
+
+const responsive = [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+    },
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+    },
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+    },
+  },
+];
 
 const NumberItem = ({ item, setTitle }) => {
   const [isOn, setIsOn] = useState(false);
@@ -17,9 +40,13 @@ const NumberItem = ({ item, setTitle }) => {
     setTitle(null);
     setIsOn(false);
   };
+  const goToSection = () => {
+    const element = document.getElementById(`section_id_${item.id}`);
+    if (element) element.scrollIntoView({ behavior: "smooth" });
+  };
 
   return (
-    <div className="number-item" onMouseOver={hover.bind(this)} onMouseLeave={leave.bind(this)} id={`section_item_${item.id}`}>
+    <div className="number-item" onMouseOver={hover.bind(this)} onMouseLeave={leave.bind(this)} id={`section_item_${item.id}`} onClick={goToSection.bind(this)}>
       {isOn ? (
         <>
           <div className="number-point strong-point"></div>
@@ -39,15 +66,15 @@ const NumberItem = ({ item, setTitle }) => {
 
 const NavNoHover = () => {
   const navHover = useRef();
+  const sliderWrapper = useRef();
   const [title, setTitle] = useState();
-  const [pos, setPos] = useState(0);
   const [showArrow, setShowArrow] = useState(false);
 
   const moveRight = () => {
-    setPos(Math.min(pos + 15, maximumMarginLeft));
+    sliderWrapper.current.slickNext();
   };
   const moveLeft = () => {
-    setPos(Math.max(0, pos - 15));
+    sliderWrapper.current.slickPrev();
   };
   const onWheel = (evt) => {
     if (evt.deltaY > 0) moveRight();
@@ -80,10 +107,12 @@ const NavNoHover = () => {
         <img src={arrow_right_png} alt="arrow-right" onMouseUp={moveRight.bind(this)} onMouseDown={moveRight.bind(this)} />
       </div>
       <div className="numbers-line" onWheel={onWheel.bind(this)}></div>
-      <div className="numbers-body" style={{ marginLeft: -pos }} onWheel={onWheel.bind(this)}>
-        {sectionItems.map((item) => (
-          <NumberItem key={item.id} item={item} setTitle={setTitle} />
-        ))}
+      <div className="numbers-body" onWheel={onWheel.bind(this)}>
+        <Slider speed={500} slidesToShow={5} slidesToScroll={1} arrows={false} ref={(r) => (sliderWrapper.current = r)} responsive={responsive} adaptiveHeight={true} centerPadding="15px">
+          {sectionItems.map((item) => (
+            <NumberItem key={item.id} item={item} setTitle={setTitle} />
+          ))}
+        </Slider>
       </div>
     </div>
   );
